89f7a27476646c56d6c6d6ed49797c4b9429eb03
honorable-scraper-2425 <kdraper93@gmail.com>
2019-06-28 04:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-var name = 'Katie'
 
-// Add your functions and code here
-function destructivelyAppendKitten(name)
+var name = 'Katie'
